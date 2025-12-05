a080d9c167a5cbb3952219c33e3f55b1d9f1b0ae
Helena <helenarigoni@ufpr.br>
2025-12-05 16:45:07 -0300

    Correção de historico.php

--- jogo/historico.php
+++ jogo/historico.php
@@ -6,6 +6,7 @@ check_user_logged_in();
 
 $id_usuario = $_SESSION['id_usuario'];
 
+// coleta os dados da tabela 'table_matches'
 $stmt = $pdo->prepare("SELECT pontos, jogado FROM table_matches WHERE id_usuario = ? ORDER BY jogado DESC");
 $stmt->execute([$id_usuario]);
 $matches = $stmt->fetchAll();
@@ -21,30 +22,36 @@ $matches = $stmt->fetchAll();
 <body>
   <div class="container m-4">
     <h1>Histórico de Voluntariado</h1>
-    <a href="inicio.php">Voltar ao Painel</a>
-    <hr>
-    <table class="table table-stripped table-hover">
-      <thead>
-        <tr>
-          <th>Data e Hora</th>
-          <th>Pontuação</th>
-        </tr>
-      </thead>
-      <tbody>
-        <?php if (empty($matches)): ?>
-          <tr>
-            <td colspan="2" class="text-center">Você ainda não jogou nenhuma partida. Os gatinhos estão te esperando, vamos!</td>
-          </tr>
-        <?php else: ?>
-          <?php foreach ($matches as $match): ?>
-            <tr>
-              <td><?php echo date('d/m/y H:i:s', strtotime($match['jogado'])); ?></td>
-              <td><?php echo $match['pontos']; ?></td>
-            </tr>
-          <?php endforeach; ?>
-        <?php endif; ?>
-      </tbody>
-  </table>
-</div>
-</body>
-</html>
+    <a href="inicio.php" class="btn btn-secondary mb-3">Voltar ao Painel</a>
+    
+    <div class="card"
+         <div class="card-body">
+             <table class="table table-striped table-hover">
+                 <thead class="table-dark">
+                     <tr>
+                         <th>Data e Hora</th>
+                         <th>Pontuação</th>
+                     </tr>
+                 </thead>
+                 <tbody>
+                     <?php if (empty($matches)): ?>
+                     <tr>
+                         <td colspan="2" class="text-center p-4">
+                             <strong>Você ainda não jogou uma partida.</strong>
+                             Os gatinhos estão te esperando!
+                         </td>
+                     </tr>
+                     <?php else: ?>
+                      <?php foreach ($matches as $match): ?>
+                       <tr> 
+                           <td><?php echo date('d/m/Y H:i', strtotime($match['jogado'])); ?></td>
+                           <td><span class="badge bg-primary rounded-pill"><?php echo $match['pontos']; ?> pts</span></td>
+                     </tr>
+                    <?php endforeach; ?>
+                   <?php endif; ?>
+                 </tbody> 
+             </table>
+        </div>
+  </div>
+  </body>
+  </html>
