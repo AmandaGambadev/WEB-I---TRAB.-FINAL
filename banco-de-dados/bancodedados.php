cb5ca7501af31a099a19935eabc86fa4c1eab64f
Helena <helenarigoni@ufpr.br>
2025-12-04 22:51:35 -0300

    Implementação do PDO em bancodedados.php

--- banco-de-dados/bancodedados.php
+++ banco-de-dados/bancodedados.php
@@ -5,20 +5,24 @@ $db_name = "shelter_cats";
 $db_user = "root";
 $db_password = "";
 
-if (session_status() === PHP_SESSION_NONE) {  // inicia a sessão
-  session_start();
-}
+ if (session_status() === PHP_SESSION_NONE) {  // inicia a sessão
+   session_start();
+ }
 
-$mysqli = new mysqli($db_host, $db_user, $db_password, $db_name);  // conexão com mysqli
+ try {
+     // criação da conexão pdo
+     $pdo = new
+ PDO("mysql:host=$db_host;dbname=$db_name;charset=utf8mb4", $db_user, $db_password);
+    
+     // em caso de erro SQL
+     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+     // definição do retorno padrão
+     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    
+  } catch (Exception $ex) {
+      // interrupção segura caso a conexão falhe 
+      die("Erro! Não foi possível se conectar com o banco de dados. Tente novamente mais tarde.");
+  }
 
-if ($mysqli->connect_error) {
-  die("Erro! Não foi possível se conectar com o banco de dados: " . $mysqli->connect_error);
-}
-
-$mysqli->set_charset("utf8mb4");
-
-// Criar conexão PDO para arquivos que usam PDO
-$pdo = new PDO("mysql:host=$db_host;dbname=$db_name;charset=utf8mb4", $db_user, $db_password);
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-?>
+ ?>
