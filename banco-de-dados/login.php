47e03abd4d9d6f13773db472297635e4a8912ffe
Helena <helenarigoni@ufpr.br>
2025-12-04 22:52:58 -0300

    Correção de login.php

--- banco-de-dados/login.php
+++ banco-de-dados/login.php
@@ -1,50 +1,49 @@
 <?php
 
-require_once 'bancodedados.php';
+ require_once 'bancodedados.php'; // conexão pdo
 
-if ($_SERVER['REQUEST_METHOD'] !== 'POST') {   // verifica o método
-  header('Location: ../jogo/index.php');
-  exit();
-}
-
-$username = $_POST['username'] ?? '';  // validação do login
-$password = $_POST['password'] ?? '';
-
-if (empty($username) || empty($password)) {
-  header('Location: ../jogo/index.php?error=empty_fields');
-  exit();
-}
-
-$stmt = $mysqli->prepare("SELECT id_usuario, user_usuario, senha FROM table_users WHERE user_usuario = ?");  // consulta sql
-
-if ($stmt === false) {
-  die("Erro! Não foi possível fazer a consulta: " . $mysqli->error);
-}
-
-$stmt->bind_param("s", $username);  // "s" indica que é string
-
-if ($stmt->execute()) {  // a consulta é feita
-  $result = $stmt->get_result();  // resultado
-
-  $user = $result->fetch_assoc();
-
-  if ($user && password_verify($password, $user['senha'])) {  // verifica username e senha
-    session_regenerate_id(true);
-    $_SESSION['id_usuario'] = $user['id_usuario'];
-    $_SESSION['user_usuario'] = $user['user_usuario'];
-
-    header('Location: ../jogo/inicio.php');
-    exit();
-  } else {
-    header('Location: ../jogo/index.php?error=invalid_login');
-    exit();
-  }
-} else {
-  header('Location: ../jogo/index.php?error=db_error');
-  exit();
-}
-
-$stmt->close();
-$mysqli->close();
-
-?>
+ if ($_SERVER['REQUEST METHOD'] !== 'POST') {
+     header('Location: ../jogo/index.php');
+     exit();
+ }
+ 
+ $username = $_POST['username'] ?? '';
+ $password = $_POST['password'] ?? '';
+ 
+ // validação
+ if (empty($username) || empty($password)) {
+     header('Location: ../jogo/index.php?error=empty_fields');
+     exit();
+ }
+ 
+ // consulta com pdo em $stmt
+ try {
+     $stmt = $pdo->prepare("SELECT id_usuario, user_usuario, senha FROM table_users WHERE user_usuario = ?");
+     
+     // execução com array
+     $stmt->execute([$username]);
+     
+     // busca a resposta 
+     $user = $stmt->fetch();
+     
+     // checa se o usuário e senha existem/estão certos
+     if ($user && password_verify($password, $user['senha'])) {
+         // faz login
+         session_regenerate_id(true); // novo ID usado para abrir o jogo
+         $_SESSION['id_usuario'] = $user['id_usuario'];
+         $_SESSION['user_usuario'] = $user['user_usuario'];
+         
+         header('Location: ../jogo/inicio.php');
+         exit();
+     } else {
+         // se o usuário/senha não existem/estão incorretos
+         header('Location: ../jogo/index.php?error=invalid_login');
+         exit();   
+     }
+     
+ } catch (PDOException $e) {
+     // se o bd der erro
+     header('Location: ../jogo/index.php?error=db_error');
+     exit();
+ }
+ ?> 
